282220ca9b9fba1642bc6ec9ff973b54101afbdc
sammy1412 <136956293+sammy1412@users.noreply.github.com>
2023-06-25 09:25:37 +0700

    Add files via upload

---
+++ Class Python buoi 1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **Cach dung jupyter notebook**
+
+# In[12]:
+
+
+#in dong o man hinh
+print('hello world')
+print("hello world")
+print("""hello world""")
+print('''hello world,
+
+click here to start''')
+
+
+# In[13]:
+
+
+a=10
+print(a)
+
+
+# In[14]:
+
+
+a=a*2
+print(a)
+
+
+# **cell markdown**
+
+# # h1
+# ## h2
+# ### h3
+# #### h4
+# ##### h5
+# ###### h6
+
+# *in nghiêng*
+# 
+
+# In[18]:
+
+
+pwd
+
+
+# In[ ]:
+
+
+
+
